cc27afed61376de21ca56957249dd0ae916a49b2
Xinyu Tu <carson_tu@hotmail.com>
2025-07-27 02:24:31 -0400

    f

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import About from '../components/About';
-import Team from '../components/Team';
 
 const AboutPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-white">
       <About />
-      <Team />
+      {/* 移除了 Team 组件，避免显示虚假的团队信息 */}
     </div>
   );
 };
